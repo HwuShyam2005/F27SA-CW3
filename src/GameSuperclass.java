bb77c02ce6ecea353bc0d1c2cec010f11d88414c
Thomas Methven <t.methven.hw.ac.uk>
2022-10-19 11:57:40 +0100

    Initial commit

---
+++ src/GameSuperclass.java
@@ -0,0 +1,11 @@
+
+public abstract class GameSuperclass
+{
+	public GameSuperclass(String gameName)
+	{
+		
+	}
+	
+	abstract public void printGameIntro();
+	abstract public boolean processInput(int value);
+}
---
+++ src/MainClass.java
@@ -0,0 +1,26 @@
+public class MainClass
+{
+	private static GameSuperclass game;
+	
+	public static void main(String[] args)
+	{
+		//All your user input code should go in main
+		//Including looping until the game selected
+		//tells you that it is complete via the return
+		//value of the relevant method.
+	}
+
+	/*private static boolean isInteger(String value)
+	{
+		//You need to complete this method - you can comment it out
+		//until you need it!
+		try
+		{
+
+		}
+		catch()
+		{
+
+		}
+	}*/
+}
